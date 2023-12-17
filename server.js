7f154317b4985f07b12aed7f1a69a3d9c78d5f69
Jeff McDonald <jwmcdonald85@gmail.com>
2023-12-17 12:04:37 -0500

    Magic 8 Ball

--- server.js
+++ server.js
@@ -42,3 +42,19 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
 // This function will run after the server turns on and return what port is listening.
 app.listen(1000, () => {console.log("The server is listening on port 1000")});
 
+
+// --------------- ***** Magic 8 Ball ***** --------------- //
+
+// 2. Magic Route
+
+app.get("/magic/:question", (req, res) => {
+    // Array of possible responses
+    const response = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    // Randomizer 
+    const rand = Math.ceil(Math.random() * response.length)
+    const randResponse = response[rand]
+    // Question param
+    const question = req.params.question
+    res.send(`<h1>${question}</h1>
+              <h1>${randResponse}</h1>`)
+})
