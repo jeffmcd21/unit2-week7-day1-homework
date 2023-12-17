4c41f8064a4bfd5028a5323c1d8e540f5d6cd4ed
Jeff McDonald <jwmcdonald85@gmail.com>
2023-12-17 12:15:48 -0500

    Setting up Beer Server

---
+++ pass-it-around/server.js
@@ -0,0 +1,21 @@
+
+// --------------- ***** Take one Down and Pass it Around ***** --------------- //
+
+const express = require("express")
+const app = express()
+
+
+
+
+
+
+
+
+
+
+
+
+// Server listener
+app.listen(99, () => {
+    console.log("listening on port 99")
+})
